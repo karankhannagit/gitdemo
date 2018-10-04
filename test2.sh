5b9d5412338dd1572da7fa26dfa1066f76c5d1fd
karan_nclouds <karan.khanna@nclouds.com>
2018-10-04 13:45:54 +0530

    multiple commits

--- test2.sh
+++ test2.sh
@@ -1,10 +1,25 @@
-declare -a val="$(sudo git diff-tree --no-commit-id --name-only -r $(sudo git log -1 --pretty=%H))"
-
-IFS='/' read -ra ADDR <<<  $val
-for i in "${ADDR[@]}"; do
-    echo  "$i"
-    if [ $i = "folder5" ]; then
+#!/bin/bash
+#GIT_COMMIT
+c=0
+declare -a valueset
+val="$(sudo git diff-tree --no-commit-id --name-only -r $(sudo git log -1 --pretty=%H))"
+dataarr=$(echo $val | tr " " "\n")
+#IFS=" " read -ra ADDR1 <<<  $val
+for path in $dataarr; do
+  IFS='/' read -ra ADDR <<<  $path
+  for i in "${ADDR[@]}"; do
+    #echo  "$i"s
+    if [ $i = "folder2" ]; then
         echo "running kong"
-        exit
+        valueset[c]=$i
+        c=$((c+1));
+        #exit
+    else
+      echo $i
     fi
+  done
+done
+
+for values in "${valueset[@]}"; do
+  echo "data -------- "$values
 done
